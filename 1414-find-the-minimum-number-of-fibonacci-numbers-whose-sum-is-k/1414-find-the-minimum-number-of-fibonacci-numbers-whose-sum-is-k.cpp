7f2bc4ffe46d02726cde0142a82424114b7057e3
Keshav Kankale <keshavkankale@gmail.com>
2026-08-02 03:20:42 +0530

    Time: 0 ms (100%), Space: 8.4 MB (82.06%) - LeetHub

--- 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.cpp
+++ 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    int findMinFibonacciNumbers(int k) {
-        vector<int> fib;
-        fib.push_back(1);
-        fib.push_back(1);
-        
-        int a = 1, b = 1;
-        while (true) {
-            int next_fib = a + b;
-            if (next_fib > k) {
-                break;
-            }
-            fib.push_back(next_fib);
-            a = b;
-            b = next_fib;
+    int findMinFibonacciNumbers(long long  k) {
+        vector<long long >fibo(45,0);
+        fibo[0]=1;
+        fibo[1]=1;
+        for(int i=2;i<45;i++)
+        {
+            fibo[i]=fibo[i-1]+fibo[i-2];
         }
-        
-        int count = 0;
-        int n = fib.size();
-        
-        for (int i = n - 1; i >= 0; i--) {
-            if (fib[i] <= k) {
-                k -= fib[i];  
-                count++;    
+        cout<<fibo.back()<<endl;
+        int cnt=0;
+        while(k>0)
+        {
+            auto p=lower_bound(fibo.begin(),fibo.end(),k);
+            if(*p==k)
+            {
+                return cnt+1;
             }
-            if (k == 0) {
-                break;
-            }
-        }
-        
-        return count;
-    }
+            else{
+            
+            p--;
+            cnt++;
+            k-=*p;}
 
+        }
+        return cnt;
+        
+    }
 };
