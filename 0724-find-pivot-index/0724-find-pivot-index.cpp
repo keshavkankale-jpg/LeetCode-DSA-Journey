b09bb53bc5a83f8bd769859ce951199542c8a71f
Keshav Kankale <keshavkankale@gmail.com>
2026-09-06 11:47:45 +0530

    Time: 0 ms (100%), Space: 35.6 MB (79.06%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n=nums.size();
-        
-        int right_sum=0;
-        for(int i=0; i<n; i++){
-            right_sum+=nums[i];
-        }
         int left_sum=0;
-
+        int total_sum=0;
         for(int i=0; i<n; i++){
-            if(left_sum==right_sum-nums[i]-left_sum){
+            total_sum+=nums[i];
+        }
+        for(int i=0; i<n; i++){
+            int right_sum=total_sum-left_sum-nums[i];
+            if(left_sum==right_sum){
                 return i;
             }
             left_sum+=nums[i];
         }
         return -1;
+       
         
     }
 };
