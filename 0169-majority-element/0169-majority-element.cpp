fd62424fea1b8c2236b1d150478bc9d9e590bc22
Keshav Kankale <keshavkankale@gmail.com>
2026-09-07 14:51:39 +0530

    Time: 0 ms (100%), Space: 28.3 MB (44.24%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,17 +3,22 @@ public:
     int majorityElement(vector<int>& nums) {
         int n=nums.size();
 
-        unordered_map<int,int> mpp;
+       int element=0;
+       int count=0;
 
-        for(int i=0; i<n; i++){
-            mpp[nums[i]]++;
+       for(int i=0; i<n; i++){
+        if(count==0){
+            element=nums[i];
+            count=1;
         }
-        for(int i=0; i<n; i++){
-            if(mpp[nums[i]]>(n/2)){
-                return nums[i];
-            }
+        else if(element==nums[i]){
+            count++;
         }
-        return -1;
+        else{
+            count--;
+        }
+       }
+       return element;
         
     }
 };
